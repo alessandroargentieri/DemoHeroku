1920505064fbc2c37e6b55bb4f7af2339e36760b
Alessandro Argentieri <alessandroargentieri@libero.it>
2017-12-18 15:42:39 +0100

    test2

--- src/main/java/com/quicktutorialz/learnmicroservices/DemoHeroku/daos/AuthorDao.java
+++ src/main/java/com/quicktutorialz/learnmicroservices/DemoHeroku/daos/AuthorDao.java
@@ -4,4 +4,5 @@ import com.quicktutorialz.learnmicroservices.DemoHeroku.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorDao extends JpaRepository<Author, Integer>{
+
 }
